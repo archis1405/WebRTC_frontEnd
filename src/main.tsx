da873bdbbab7dbb4bddb6ad04c4ce8f5b64ff074
archis1405 <archismandas1405@gmail.com>
2026-01-31 04:03:26 +0530

    Added the basic WebRTC socket connection from frontend

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+import { SocketProvider } from './Context/socketContext.tsx'
+
+createRoot(document.getElementById('root')!).render(
+  <SocketProvider>
+    <App />
+  </SocketProvider>
+    
+  
+)
